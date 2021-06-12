c5f3280b7fbf4c063d1785c7ed4a87d2199763b0
Kirill Gladkikh <onest.kill@yandex.ru>
2021-06-12 16:57:06 +0300

    new sprint

---
+++ src/components/EditProfileAvatar.js
@@ -0,0 +1,44 @@
+import { createRef } from "react";
+import PopupWithForm from "./PopupWithForm";
+
+const EditProfileAvatar = (props) => {
+  const avatarRef = createRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const avatar = avatarRef.current.value;
+
+    props.onUpdateAvatar({
+      avatar,
+    });
+  };
+
+  return (
+    <PopupWithForm
+      name="update-avatar"
+      title="Обновить аватар"
+      isOpen={props.isOpen}
+      onClose={props.onClose}
+      onSubmit={handleSubmit}
+      children={
+        <>
+          <input
+            type="url"
+            className="popup__field"
+            name="avatar"
+            placeholder="Ссылка"
+            ref={avatarRef}
+            required
+          />
+          <span className="popup__error"></span>
+          <button type="submit" className="popup__submit-button">
+            Сохранить
+          </button>
+        </>
+      }
+    />
+  );
+};
+
+export default EditProfileAvatar;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,42 +1,64 @@
-import Card from './Card';
+import { useContext } from "react";
+import currentUserContext from "../context/CurrentUserContext";
+
+import Card from "./Card";
 
 const Main = (props) => {
-	return (
-		<>
-			<section className="profile">
-				<div className="profile__content">
-					<div className="profile__avatar-overlay" onClick={props.onEditAvatar}>
-						<img className="profile__avatar" src={props.userAvatar} alt="Фото" />
-					</div>
-					<div className="profile__info">
-						<div className="profile__item">
-							<div className="profile__item-inner">
-								<h1 className="profile__title">{props.userName}</h1>
-								<button type="button" id="edit-popup" className="profile__edit-button" onClick={props.onEditProfile}></button>
-							</div>
-						</div>
-						<p className="profile__subtitle">{props.userDescription}</p>
-					</div>
-				</div>
-				<button type="button" id="add-popup" className="profile__add-button" onClick={props.onAddPlace}></button>
-			</section>
+  const currentUser = useContext(currentUserContext);
 
-			<section className="elements">
-				<ul className="element">
-					{props.cards.map((item) =>
-						<Card
-							key={item._id}
-							image={item.link}
-							name={item.name}
-							likesCount={item.likes.length}
-							onCardClick={() => props.onCardClick(item)}
-						/>
-					)}
-				</ul>
-			</section>
-		</>
-	)
+  return (
+    <>
+      <section className="profile">
+        <div className="profile__content">
+          <div className="profile__avatar-overlay" onClick={props.onEditAvatar}>
+            <img
+              className="profile__avatar"
+              src={currentUser.avatar}
+              alt="Фото"
+            />
+          </div>
+          <div className="profile__info">
+            <div className="profile__item">
+              <div className="profile__item-inner">
+                <h1 className="profile__title">{currentUser.name}</h1>
+                <button
+                  type="button"
+                  id="edit-popup"
+                  className="profile__edit-button"
+                  onClick={props.onEditProfile}
+                ></button>
+              </div>
+            </div>
+            <p className="profile__subtitle">{currentUser.about}</p>
+          </div>
+        </div>
+        <button
+          type="button"
+          id="add-popup"
+          className="profile__add-button"
+          onClick={props.onAddPlace}
+        ></button>
+      </section>
 
-}
+      <section className="elements">
+        <ul className="element">
+          {props.cards.map((item) => (
+            <Card
+              key={item._id}
+              owner={item.owner}
+              image={item.link}
+              name={item.name}
+              likes={item.likes}
+              likesCount={item.likes.length}
+              onCardClick={() => props.onCardClick(item)}
+              onLikeClick={() => props.onCardLike(item)}
+              onDeleteClick={() => props.onCardDelete(item)}
+            />
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+};
 
 export default Main;
